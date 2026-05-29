11a255bcd261833847ce97e8162a23e8e6299c28
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-05-29 12:10:39 +0200

    fix LCSF algorithm fix

--- src/algorithm/lcsf_algorithm.py
+++ src/algorithm/lcsf_algorithm.py
@@ -1,8 +1,13 @@
 from src.algorithm.sheduling_algorithm import ShedulingAlgorithm
 
 class LCSFalgorithm(ShedulingAlgorithm):
+    def __init__(self):
+            self.current_process = None
     def choose(self, ready_queue):
         if not ready_queue:
             return None
+        if self.current_process in ready_queue:
+           return self.current_process  
         else:
-            return ready_queue[-1]
+            self.current_process = ready_queue[-1]
+            return self.current_process
